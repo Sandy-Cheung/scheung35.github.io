44d5106e1b817bd1c48314d16b3243d4e719b86d
Sandy-Cheung <scheung35@myseneca.ca>
2022-04-13 04:28:54 -0400

    added new files

---
+++ misc/my_js.js
@@ -0,0 +1,27 @@
+function show_hiring() {
+    var hourly_label = document.querySelector("#hourly_label");
+    var hourly_input = document.querySelector("#hourly");
+    hourly_input.type = "number";
+    hourly_input.min = 15;
+    hourly_input.max = 90;
+    hourly_input.step = 0.1;
+    hourly_label.setAttribute("for", "hourly");
+    hourly_label.style.display = "block";
+}
+
+function hide_hiring() {
+    var hourly_label = document.querySelector("#hourly_label");
+    var hourly_input = document.querySelector("#hourly");
+    hourly_label.style.display = "none";
+    hourly_input.type = "hidden";
+}
+
+function go_to(anchor) {
+    var place = document.location.toString().split('#')[0];
+    document.location = place + '#' + anchor;
+    return false;
+}
+
+function download() {
+    window.open('misc/resume.docx');
+}
